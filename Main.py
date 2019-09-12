398c73c2409e379b2935c3af3d1e1ad8e639e07b
Benton Stacy <bmstacy7127@eagle.fgcu.edu>
2019-09-12 14:11:56 -0400

    Initial commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+# Benton Stacy 09/12/2019
+# Description: __________
+#   Requirements:
+#   1. add your name as a comment at top of every file
+#   2. add a description of the program as a comment at top
+#   3. simple I/O: add a greeting / introduction to the user
+#   4. basic computation: use arithmetic operators
+
+print("Hello and welcome!")
