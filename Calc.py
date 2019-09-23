3c208fbfc2187a916b831f1cdb204865e91a4d5a
bstacy0015 <54415436+bstacy0015@users.noreply.github.com>
2019-09-23 00:00:39 -0400

    Add files via upload

--- Calc.py
+++ Calc.py
@@ -1,5 +1,5 @@
-# Benton Stacy 09/12/2019 - 09/17/2019
-# Description: A calculator.
+# Benton Stacy 09/12/2019 - 09/22/2019
+# Description: A calculator with an RNG function if you can't think of a number.
 #   Requirements:
 #   1. add your name as a comment at top of every file
 #   2. add a description of the program as a comment at top
@@ -8,16 +8,26 @@
 
 print("Hello and welcome!")
 
+# Import the RNG
+import random
+
 # Input -> store num1 as int (from str)
 num1 = input("Enter first number: ")
-num1 = int(num1)
+if num1 == "rand":
+    num1 = random.randint(-100, 100)
+else:
+    num1 = int(num1)
 
 # Input -> store operation
 operation = input("Enter the operation: ")
 
 # Input -> store num2 as int (from str)
 num2 = input("Enter second number: ")
-num2 = int(num2)
+if num2 == "rand":
+    num2 = random.randint(-100, 100)
+else:
+    num2 = int(num2)
+
 print("Processing.")
 
 # Conditional for analysis of operation
